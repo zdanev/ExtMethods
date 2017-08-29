7302c25a4a9ca972795eac9eaf3dcc83f72626a6
Zdravko Danev <zdanev@gmail.com>
2017-08-29 13:18:57 -0700

    Initial commit

---
+++ StrExt.Tests.cs
@@ -0,0 +1,44 @@
+using System;
+using Xunit;
+
+namespace ExtMethods
+{
+    public class StrExt_Tests
+    {
+        [Fact]
+        public void String_FormatWith()
+        {
+            // arrange
+
+            // act
+            var result = "abc {0} def {1}".FormatWith("xxx", 7);
+
+            // assert
+            Assert.Equal("abc xxx def 7", result);
+        }
+
+        [Fact]
+        public void String_Left()
+        {
+            // arrange 
+
+            // act
+            var result = "xxxabc".Left(3);
+
+            // assert
+            Assert.Equal("xxx", result);
+        }
+
+        [Fact]
+        public void String_Right()
+        {
+            // arrange 
+
+            // act
+            var result = "xxxabc".Right(3);
+
+            // assert
+            Assert.Equal("abc", result);
+        }
+    }
+}
---
+++ StrExt.cs
@@ -0,0 +1,20 @@
+namespace ExtMethods
+{
+    public static class StrExt
+    {
+        public static string FormatWith(this string format, params object[] args)
+        {
+            return string.Format(format, args);
+        }
+
+        public static string Left(this string s, int n)
+        {
+            return s.Substring(0, n);
+        }
+
+        public static string Right(this string s, int n)
+        {
+            return s.Substring(s.Length - n, n);
+        }
+    }
+}
